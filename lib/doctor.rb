c9d30b25dceaef8be0781a1a0185674f650f280b
Drake Rentrop <drakerentrop@gmail.com>
2019-07-16 18:29:18 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,6 +17,13 @@ def new_appointment(date, patient)
   Appointment.new(date, patient, self)
 end
 
+def appointments
+  Appointments.all.select do |appointments|
+    appointments.dotor == self
+  end
+end
+
+
 def patients
   Appointments.all.select do |appointment|
     appointment.patients == self
