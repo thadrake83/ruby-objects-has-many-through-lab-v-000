a9bcb21e6b0817b85f276a5e378710df4de584a0
Drake Rentrop <drakerentrop@gmail.com>
2019-07-16 18:09:59 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,16 +2,18 @@ class Doctor
 
  attr_accessor :name, :appointment, :patient
 
- @@all_doc = []
+ @@all = []
 
  def initialize(name)
   @name = name
-  @@all_doc << self
+  @@all << self
  end
 
  def self.all
-   @@all_doc
+   @@all
  end
 
+def new_appointment(patient, date)
+  Appointment.new()
 
  end
